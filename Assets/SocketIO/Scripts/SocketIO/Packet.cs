f6494c4087c283e56ee054e568f1d200ce08e6ff
loidinhm31 <loidinh.git@gmail.com>
2024-04-08 10:47:18 +0700

    Fix socketio was not send connect command

--- Assets/SocketIO/Scripts/SocketIO/Packet.cs
+++ Assets/SocketIO/Scripts/SocketIO/Packet.cs
@@ -41,6 +41,8 @@ namespace SocketIO
 		public Packet() : this(EnginePacketType.UNKNOWN, SocketPacketType.UNKNOWN, -1, "/", -1, null) { }
 		public Packet(EnginePacketType enginePacketType) : this(enginePacketType, SocketPacketType.UNKNOWN, -1, "/", -1, null) { }
 
+		public Packet(EnginePacketType enginePacketType, SocketPacketType socketPacketType) : this(enginePacketType, socketPacketType, -1, "/", -1, null) { }
+
 		public Packet(EnginePacketType enginePacketType, SocketPacketType socketPacketType, int attachments, string nsp, int id, JSONObject json)
 		{
 			this.enginePacketType = enginePacketType;
